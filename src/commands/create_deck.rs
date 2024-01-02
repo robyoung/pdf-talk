805b33194ac48c0749235691361c02b9ce488350
Rob Young <rob@robyoung.digital>
2024-01-02 22:17:23 +0000

    Add content builder

--- src/commands/create_deck.rs
+++ src/commands/create_deck.rs
@@ -1,16 +1,61 @@
-use lopdf::{dictionary, Document, Object};
+use lopdf::{dictionary, Document, Object, Stream};
 
-use crate::{config::CreateConfig, document::DocumentAdditions};
+use crate::{
+    config::CreateConfig,
+    document::{text, text_item as t, DocumentAdditions},
+    fonts::{self, FontReference},
+};
 
 pub fn main(config: CreateConfig) {
-    let mut doc = Document::with_version("2.0");
+    let mut doc = Document::with_version("1.7");
+
+    let font_data = std::fs::read("assets/Georgia.ttf").expect("could not read font file");
+    let font_ref = fonts::true_type(&font_data).add_to_doc(&mut doc);
 
     let pages_id = doc.new_object_id();
+
+    let resources_id = doc.add_object(dictionary! {
+        "Font" => dictionary! {
+            "F1" => font_ref.object_id(),
+        }
+    });
+    let content = text()
+        .font("F1", 36)
+        .move_to(100, 200)
+        .leading(48)
+        .word_spacing(-8f32)
+        .colour((0.106, 0.259, 0.471))
+        .text(vec![
+            t("W"),
+            t(-500),
+            t("h"),
+            t(-50),
+            t("at even i"),
+            t(200),
+            t("s"),
+            t(" a P"),
+            t(-100),
+            t("D"),
+            t(-200),
+            t("F"),
+            t(-100),
+            t("?"),
+        ])
+        .build_content();
+    let content_id = doc.add_object(Stream::new(dictionary! {}, content.encode().unwrap()));
+    let page_id = doc.add_object(dictionary! {
+        "Type" => "Page",
+        "Parent" => pages_id,
+        "Contents" => content_id,
+    });
+
     let pages = dictionary! {
         "Type" => "Pages",
-        "Kids" => vec![],
-        "Count" => 0,
+        "Kids" => vec![page_id.into()],
+        "Count" => 1,
+        "Resources" => resources_id,
         "MediaBox" => vec![0.into(), 0.into(), 960.into(), 540.into()],
+        // "MediaBox" => vec![0.into(), 0.into(), 960.into(), 540.into()],
     };
     doc.objects.insert(pages_id, Object::Dictionary(pages));
     doc.add_catalog(pages_id);
--- src/document.rs
+++ src/document.rs
@@ -1,4 +1,7 @@
-use lopdf::{dictionary, Document, ObjectId};
+use lopdf::{
+    content::{Content, Operation},
+    dictionary, Document, Object, ObjectId,
+};
 
 pub(crate) trait DocumentAdditions {
     fn add_catalog(&mut self, pages_id: ObjectId) -> ObjectId;
@@ -13,4 +16,125 @@ impl DocumentAdditions for Document {
         self.trailer.set("Root", catalog_id);
         catalog_id
     }
+}
+
+pub(crate) enum TextItem<'a> {
+    String(&'a str),
+    Position(f32),
+}
+
+pub(crate) fn text_item<'a, T: Into<TextItem<'a>>>(t: T) -> TextItem<'a> {
+    t.into()
+}
+
+impl<'a> Into<Object> for TextItem<'a> {
+    fn into(self) -> Object {
+        match self {
+            TextItem::String(s) => Object::string_literal(s),
+            TextItem::Position(p) => Object::Real(p),
+        }
+    }
+}
+
+impl<'a> From<&'a str> for TextItem<'a> {
+    fn from(value: &'a str) -> Self {
+        Self::String(value)
+    }
+}
+
+impl<'a> From<f32> for TextItem<'a> {
+    fn from(value: f32) -> Self {
+        Self::Position(value)
+    }
+}
+
+impl<'a> From<i32> for TextItem<'a> {
+    fn from(value: i32) -> Self {
+        Self::Position(value as f32)
+    }
+}
+
+///
+/// ```
+/// text()
+///     .font("F1", 36)
+///     .move_to(100, 200)
+///     .leading(48)
+///     .word_spacing(-8)
+///     .colour((0.106, 0.259, 0.471))
+///     .text([
+///         "W".into(),
+///         "h".into(),
+///         (-500).into(),
+///         "at even i".into(),
+///         200.into(),
+///     ])
+///     .build()
+/// ```
+#[derive(Default)]
+pub(crate) struct TextBuilder {
+    operations: Vec<Operation>,
+}
+
+impl<'a> TextBuilder {
+    fn new() -> Self {
+        TextBuilder {
+            operations: vec![Operation::new("BT", vec![])],
+        }
+    }
+
+    pub fn font(mut self, font: &str, size: u32) -> Self {
+        self.operations
+            .push(Operation::new("Tf", vec![font.into(), size.into()]));
+        self
+    }
+
+    pub fn move_to(mut self, x: i32, y: i32) -> Self {
+        self.operations
+            .push(Operation::new("Td", vec![x.into(), y.into()]));
+        self
+    }
+
+    pub fn leading(mut self, l: u32) -> Self {
+        self.operations.push(Operation::new("TL", vec![l.into()]));
+        self
+    }
+
+    pub fn word_spacing(mut self, s: f32) -> Self {
+        self.operations.push(Operation::new("Tw", vec![s.into()]));
+        self
+    }
+
+    pub fn colour(mut self, c: (f32, f32, f32)) -> Self {
+        self.operations.push(Operation::new(
+            "rg",
+            vec![c.0.into(), c.1.into(), c.2.into()],
+        ));
+        self
+    }
+
+    pub fn text(mut self, text: Vec<TextItem>) -> Self {
+        self.operations.push(Operation::new(
+            "TJ",
+            vec![Object::Array(
+                text.into_iter().map(Into::into).collect::<Vec<Object>>(),
+            )],
+        ));
+        self
+    }
+
+    pub fn build_operations(mut self) -> Vec<Operation> {
+        self.operations.push(Operation::new("ET", vec![]));
+        self.operations
+    }
+
+    pub fn build_content(self) -> Content {
+        Content {
+            operations: self.build_operations(),
+        }
+    }
+}
+
+pub(crate) fn text() -> TextBuilder {
+    TextBuilder::new()
 }
