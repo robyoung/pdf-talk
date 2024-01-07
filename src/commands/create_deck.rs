ad7f9f543cba95f8bc587f666745080de8c7f427
Rob Young <rob@robyoung.digital>
2024-01-07 00:34:30 +0000

    Add an image

--- src/commands/create_deck.rs
+++ src/commands/create_deck.rs
@@ -1,8 +1,8 @@
-use lopdf::{dictionary, Document, Object};
+use lopdf::{dictionary, xobject, Document, Object};
 
 use crate::{
     config::CreateConfig,
-    document::{content, DocumentAdditions},
+    document::{ContentBuilder, DocumentAdditions, IdMap},
     fonts::{FontMap, FontType0Builder},
 };
 
@@ -18,17 +18,31 @@ pub fn main(config: CreateConfig) {
     let pages_id = doc.new_object_id();
 
     let font_map = FontMap::with_one("F1", font_ref);
+    let image_stream = xobject::image("assets/tnt-logo.png").expect("could not read tnt logo");
+    let image_id = doc.add_object(image_stream);
+
+    let image_map = IdMap::with_one("Im1", image_id);
 
     let resources_id = doc.add_object(dictionary! {
-        "Font" => font_map.as_dictionary()
+        "Font" => font_map.as_dictionary(),
+        "XObject" => image_map.as_dictionary(),
     });
 
-    let content_id = content(font_map)
+    let content_id = ContentBuilder::default()
+        .with_font_map(font_map)
+        .with_xobject_map(image_map)
+        .begin_text()
         .font("F1", 36)
         .move_to(100, 200)
         .leading(48)
         .colour((0.106, 0.259, 0.471))
         .text("What even is a PDF?")
+        .end_text()
+        .save_graphics_state()
+        .cm_position(100, 100)
+        .cm_scale(118, 17)
+        .add_xobject("Im1")
+        .restore_graphics_state()
         .add_to_doc(&mut doc);
 
     let page_id = doc.add_object(dictionary! {
--- src/document.rs
+++ src/document.rs
@@ -5,8 +5,9 @@
 //! crate `printpdf`.
 use lopdf::{
     content::{Content, Operation},
-    dictionary, Document, Object, ObjectId, Stream,
+    dictionary, Dictionary, Document, Object, ObjectId, Stream,
 };
+use std::collections::HashMap;
 
 use crate::fonts::{FontMap, FontReference};
 
@@ -77,26 +78,51 @@ impl<'a> From<i32> for TextItem<'a> {
 pub(crate) struct ContentBuilder {
     operations: Vec<Operation>,
     font_map: FontMap,
+    xobjects: IdMap,
     current_font: Option<String>,
 }
 
 impl ContentBuilder {
-    fn new(font_map: FontMap) -> Self {
-        ContentBuilder {
-            operations: vec![Operation::new("BT", vec![])],
-            font_map,
-            ..Default::default()
-        }
+    pub fn with_font_map(mut self, font_map: FontMap) -> Self {
+        self.font_map = font_map;
+        self
     }
 
+    pub fn with_xobject_map(mut self, xobject_map: IdMap) -> Self {
+        self.xobjects = xobject_map;
+        self
+    }
+
+    /// Push an operation with args in the fluid interface
+    fn push(mut self, op: &str, args: Vec<Object>) -> Self {
+        self.operations.push(Operation::new(op, args));
+        self
+    }
+
+    /// Push an operation with no arguments (empty) in the fluid interface
+    fn pushe(self, op: &str) -> Self {
+        self.push(op, vec![])
+    }
+
+    /// Begin text input (`BT`)
+    pub fn begin_text(self) -> Self {
+        self.pushe("BT")
+    }
+
+    /// End text input (`ET`)
+    pub fn end_text(self) -> Self {
+        self.pushe("ET")
+    }
+
+    /// Set the text font (`Tf`)
+    ///
+    /// See section 9.3.1 of the PDF spec
     pub fn font(mut self, font: &str, size: u32) -> Self {
         if !self.font_map.contains_key(font) {
             panic!("Font {} does not exist in font map", font);
         }
         self.current_font = Some(font.to_owned());
-        self.operations
-            .push(Operation::new("Tf", vec![font.into(), size.into()]));
-        self
+        self.push("Tf", vec![font.into(), size.into()])
     }
 
     fn current_font_ref(&self) -> &Box<dyn FontReference> {
@@ -108,51 +134,121 @@ impl ContentBuilder {
             )
     }
 
-    pub fn move_to(mut self, x: i32, y: i32) -> Self {
-        self.operations
-            .push(Operation::new("Td", vec![x.into(), y.into()]));
-        self
+    /// Move to start of the next line (`Td`)
+    ///
+    /// See section 9.4.2 of the PDF spec
+    pub fn move_to(self, x: i32, y: i32) -> Self {
+        self.push("Td", vec![x.into(), y.into()])
     }
 
-    pub fn leading(mut self, l: u32) -> Self {
-        self.operations.push(Operation::new("TL", vec![l.into()]));
-        self
+    /// Set the text leading (`TL`)
+    ///
+    /// See section 9.3.1 of the PDF spec
+    pub fn leading(self, l: u32) -> Self {
+        self.push("TL", vec![l.into()])
     }
 
-    pub fn colour(mut self, c: (f32, f32, f32)) -> Self {
-        self.operations.push(Operation::new(
-            "rg",
-            vec![c.0.into(), c.1.into(), c.2.into()],
-        ));
-        self
+    /// Set nonstroking colour (`rg`)
+    ///
+    /// See section 8.6.8 of the PDF spec
+    pub fn colour(self, c: (f32, f32, f32)) -> Self {
+        self.push("rg", vec![c.0.into(), c.1.into(), c.2.into()])
     }
 
+    /// Show text string as array (`TJ`)
+    ///
+    /// See section 9.4.3 of the PDF spec
     #[allow(dead_code)]
-    pub fn text_items(mut self, text: Vec<TextItem>) -> Self {
-        self.operations.push(Operation::new(
-            "TJ",
-            vec![Object::Array(
-                text.into_iter()
-                    .flat_map(|t| match t {
-                        TextItem::String(s) => self.current_font_ref().render_text(s),
-                        TextItem::Position(_) => vec![Into::<Object>::into(t)],
-                    })
-                    .collect::<Vec<Object>>(),
-            )],
-        ));
-        self
+    pub fn text_items(self, text: Vec<TextItem>) -> Self {
+        let args = vec![Object::Array(
+            text.into_iter()
+                .flat_map(|t| match t {
+                    TextItem::String(s) => self.current_font_ref().render_text(s),
+                    TextItem::Position(_) => vec![Into::<Object>::into(t)],
+                })
+                .collect::<Vec<Object>>(),
+        )];
+        self.push("TJ", args)
     }
 
-    pub fn text(mut self, text: &str) -> Self {
-        self.operations.push(Operation::new(
-            "Tj",
-            self.current_font_ref().render_text(text),
-        ));
-        self
+    /// Show text string (`Tj`)
+    ///
+    /// See section 9.4.3 of the PDF spec
+    pub fn text(self, text: &str) -> Self {
+        let text = self.current_font_ref().render_text(text);
+        self.push("Tj", text)
     }
 
-    pub fn build_operations(mut self) -> Vec<Operation> {
-        self.operations.push(Operation::new("ET", vec![]));
+    /// Save current graphics state (`q`)
+    ///
+    /// See section 8.4.2 of the PDF spec
+    pub fn save_graphics_state(self) -> Self {
+        self.pushe("q")
+    }
+
+    /// Modify transformation matrix (`cm`)
+    ///
+    /// See section 8.4.4 of the PDF spec
+    /// See section 8.3.3 for common transformations
+    pub fn modify_trans_matrix<
+        A: Into<Number>,
+        B: Into<Number>,
+        C: Into<Number>,
+        D: Into<Number>,
+        E: Into<Number>,
+        F: Into<Number>,
+    >(
+        self,
+        a: A,
+        b: B,
+        c: C,
+        d: D,
+        e: E,
+        f: F,
+    ) -> Self {
+        self.push(
+            "cm",
+            vec![
+                Number::as_object(a),
+                Number::as_object(b),
+                Number::as_object(c),
+                Number::as_object(d),
+                Number::as_object(e),
+                Number::as_object(f),
+            ],
+        )
+    }
+
+    pub fn cm_position<X: Into<Number>, Y: Into<Number>>(self, x: X, y: Y) -> Self {
+        self.modify_trans_matrix(1, 0, 0, 1, x, y)
+    }
+
+    pub fn cm_scale<X: Into<Number>, Y: Into<Number>>(self, x: X, y: Y) -> Self {
+        self.modify_trans_matrix(x, 0, 0, y, 0, 0)
+    }
+
+    pub fn cm_rotate(self, q: f32) -> Self {
+        let (rc, rs) = (q.cos(), q.sin());
+
+        self.modify_trans_matrix(rc, rs, 0f32 - rs, rc, 0, 0)
+    }
+
+    /// Paint the specified XObject (`Do`)
+    ///
+    /// This usually means insert an image.
+    /// See section 8.8 of the PDF spec
+    pub fn add_xobject(self, key: &str) -> Self {
+        if !self.xobjects.contains_key(key) {
+            panic!("Attempt to insert unknown xobject {}", key);
+        }
+        self.push("Do", vec![key.into()])
+    }
+
+    pub fn restore_graphics_state(self) -> Self {
+        self.pushe("Q")
+    }
+
+    pub fn build_operations(self) -> Vec<Operation> {
         self.operations
     }
 
@@ -170,8 +266,69 @@ impl ContentBuilder {
     }
 }
 
-pub(crate) fn content(font_map: FontMap) -> ContentBuilder {
-    ContentBuilder::new(font_map)
+pub(crate) enum Number {
+    Integer(i64),
+    Real(f32),
+}
+
+impl Number {
+    fn as_object<T: Into<Number>>(n: T) -> Object {
+        Into::<Object>::into(Into::<Number>::into(n))
+    }
+}
+
+impl From<i64> for Number {
+    fn from(value: i64) -> Self {
+        Number::Integer(value)
+    }
+}
+
+impl From<f32> for Number {
+    fn from(value: f32) -> Self {
+        Number::Real(value)
+    }
+}
+
+impl From<Number> for Object {
+    fn from(value: Number) -> Self {
+        match value {
+            Number::Integer(v) => Object::Integer(v),
+            Number::Real(v) => Object::Real(v),
+        }
+    }
+}
+
+#[derive(Default)]
+pub(crate) struct IdMap(HashMap<String, ObjectId>);
+
+impl IdMap {
+    pub fn with_one(key: &str, id: ObjectId) -> Self {
+        let mut this = Self::default();
+        this.set(key, id);
+        this
+    }
+
+    pub fn set(&mut self, key: &str, id: ObjectId) {
+        if !key.chars().all(|c| c.is_ascii_alphanumeric()) {
+            panic!(
+                "object reference must be ASCII alpha numeric but was {}",
+                key
+            );
+        }
+        self.0.insert(key.to_owned(), id);
+    }
+
+    pub fn contains_key(&self, key: &str) -> bool {
+        self.0.contains_key(key)
+    }
+
+    pub fn as_dictionary(&self) -> Dictionary {
+        let mut dict = Dictionary::new();
+        for (key, &value) in self.0.iter() {
+            dict.set(key.as_str(), value);
+        }
+        dict
+    }
 }
 
 #[cfg(test)]
@@ -182,11 +339,14 @@ mod tests {
     #[test]
     fn text_builder() {
         let font_map = FontMap::with_one("F1", InternalFontReference::default());
-        let operations = content(font_map)
+        let operations = ContentBuilder::default()
+            .with_font_map(font_map)
+            .begin_text()
             .font("F1", 36)
             .move_to(100, 200)
             .leading(36)
             .text("Some text")
+            .end_text()
             .build_operations();
 
         assert_eq!(
@@ -209,13 +369,17 @@ mod tests {
     #[should_panic]
     fn add_text_without_font() {
         let font_map = FontMap::new();
-        content(font_map).text("some text");
+        ContentBuilder::default()
+            .with_font_map(font_map)
+            .text("some text");
     }
 
     #[test]
     #[should_panic]
     fn use_font_not_in_font_map() {
         let font_map = FontMap::with_one("F1", InternalFontReference::default());
-        content(font_map).font("F2", 10);
+        ContentBuilder::default()
+            .with_font_map(font_map)
+            .font("F2", 10);
     }
 }
